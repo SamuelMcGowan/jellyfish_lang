43ac12a57ff294c8d079b3a7bdd9b1f220d3cbfc
Sam McGowan <the.sam.mcg@protonmail.com>
2022-05-06 12:51:32 +0100

    Initial type-inference support.
    Not yet integrated into the language.

--- src/compiler/ast/types.rs
+++ src/compiler/ast/types.rs
@@ -1,11 +1,30 @@
-use internment::Intern;
-
 #[derive(Clone, PartialEq, Eq)]
-pub enum Type {
-    Parameterised(TypeIdent, Vec<Type>),
-    Simple(TypeIdent),
+pub enum InferredType {
+    Builtin(BuiltinType),
     Unknown,
 }
 
 #[derive(Clone, Copy, PartialEq, Eq)]
-pub struct TypeIdent(Intern<String>);
+pub enum BuiltinType {
+    String,
+    Integer,
+    Float,
+    Bool,
+    Unit,
+}
+
+pub fn unify_types(
+    a: InferredType,
+    b: InferredType,
+) -> Result<InferredType, (InferredType, InferredType)> {
+    match (a, b) {
+        (a, InferredType::Unknown) => Ok(a),
+        (InferredType::Unknown, b) => Ok(b),
+
+        (InferredType::Builtin(a), InferredType::Builtin(b)) if a == b => {
+            Ok(InferredType::Builtin(a))
+        }
+
+        (a, b) => Err((a, b)),
+    }
+}
