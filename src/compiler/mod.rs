770c8dc3c74dab903717b1d7a8e12ff28043c65a
Sam McGowan <the.sam.mcg@protonmail.com>
2022-05-10 12:54:11 +0100

    Encapsulate passes into function.

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,6 +1,4 @@
-use crate::compiler::passes::codegen::CodeGenerator;
-use crate::compiler::passes::resolve::Resolver;
-use crate::compiler::passes::visit::Visitor;
+use crate::compiler::passes::run_passes;
 use crate::runtime::CompiledProgram;
 use crate::Source;
 
@@ -23,22 +21,16 @@ impl<'sess> CompiledProgram<'sess> {
         let lexer = Lexer::new(source.cursor());
         let parser = Parser::new(lexer, diagnostics);
 
-        let mut module_root = parser.parse();
+        let mut module = parser.parse();
         diagnostics.assert_ok()?;
 
-        let mut resolver = Resolver::new();
-        if let Err(e) = resolver.visit_module(&mut module_root) {
-            diagnostics.report(e.report());
-            return Err(());
-        }
-
-        let mut code_gen = CodeGenerator::default();
-        if let Err(e) = code_gen.visit_module(&mut module_root) {
-            diagnostics.report(e.report());
-            return Err(());
-        }
-
-        let chunk = code_gen.chunk();
+        let chunk = match run_passes(&mut module) {
+            Ok(chunk) => chunk,
+            Err(err) => {
+                diagnostics.report(err.report());
+                return Err(());
+            }
+        };
 
         Ok(Self { source, chunk })
     }
--- src/compiler/passes/mod.rs
+++ src/compiler/passes/mod.rs
@@ -1,3 +1,20 @@
+use crate::compiler::ast::Module;
+use crate::compiler::diagnostic::JlyResult;
+use crate::compiler::passes::codegen::CodeGenerator;
+use crate::compiler::passes::resolve::Resolver;
+use crate::compiler::passes::visit::Visitor;
+use crate::runtime::chunk::Chunk;
+
 pub mod codegen;
 pub mod resolve;
 pub mod visit;
+
+pub fn run_passes(module: &mut Module) -> JlyResult<Chunk> {
+    let mut resolver = Resolver::new();
+    resolver.visit_module(module)?;
+
+    let mut codegen = CodeGenerator::default();
+    codegen.visit_module(module)?;
+
+    Ok(codegen.chunk())
+}
